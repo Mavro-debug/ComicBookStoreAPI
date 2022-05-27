c1e6804100764a433f71751162fbefdd5ac9c986
Paweł Janus <82516385+PawelJanus99@users.noreply.github.com>
2022-05-27 18:52:21 +0200

    Added AuthorizationRequirement named ResourceOperationRequirement

---
+++ ComicBookStoreAPI.Domain/Authorization/Requirements/ResourceOperationRequirement.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace ComicBookStoreAPI.Domain.Authorization.Requirements
+{
+    public enum ResourceOperation
+    {
+        Create,
+        Read,
+        Update,
+        Delete
+    }
+    public class ResourceOperationRequirement : IAuthorizationRequirement
+    {
+        public ResourceOperation ResourceOperation { get; }
+
+        public ResourceOperationRequirement(ResourceOperation resourceOperation)
+        {
+            ResourceOperation = resourceOperation;
+        }
+    }
+}
