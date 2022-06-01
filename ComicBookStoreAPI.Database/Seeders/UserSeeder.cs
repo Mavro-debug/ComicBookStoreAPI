2e7f247a8e075e255a8cd9770697afd7e1738a75
Paweł Janus <82516385+PawelJanus99@users.noreply.github.com>
2022-06-02 01:25:33 +0200

    Added Seed method to UserSeeder class

--- ComicBookStoreAPI.Database/Seeders/UserSeeder.cs
+++ ComicBookStoreAPI.Database/Seeders/UserSeeder.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ComicBookStoreAPI.Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +10,35 @@ namespace ComicBookStoreAPI.Database.Seeders
 {
     public class UserSeeder
     {
+        private readonly ApplicationDbContext _dbContext;
+        private readonly UserManager<ApplicationUser> _userManager;
+        public UserSeeder(ApplicationDbContext dbContext, UserManager<ApplicationUser> userManager)
+        {
+            _dbContext = dbContext;
+            _userManager = userManager;
+        }
 
+        public async Task Seed()
+        {
+            if (_dbContext.Database.CanConnect())
+            {
+                if (!_dbContext.Users.Any())
+                {
+                    var admin = new ApplicationUser()
+                    {
+                        UserName = "Admin",
+                        Email = "admin@admin.com"
+                    };
+
+                    await _userManager.CreateAsync(admin, "Admin!99");
+
+                    var roleResoult = await _userManager.AddToRoleAsync(admin, "Administrator");
+
+                    var token = await _userManager.GenerateEmailConfirmationTokenAsync(admin);
+
+                    await _userManager.ConfirmEmailAsync(admin, token);
+                }
+            }
+        }
     }
 }
