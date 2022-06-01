6af3936f764c48661a345f0ea0c0762b490aad06
Paweł Janus <82516385+PawelJanus99@users.noreply.github.com>
2022-06-02 00:48:12 +0200

    Added GenerateTemporaryPassword to IAccountService

--- ComicBookStoreAPI.Services/AccountService.cs
+++ ComicBookStoreAPI.Services/AccountService.cs
@@ -2,6 +2,7 @@
 using ComicBookStoreAPI.Domain.Models;
 using Google.Apis.Auth;
 using Microsoft.Extensions.Configuration;
+using System.Text;
 
 namespace ComicBookStoreAPI.Services
 {
@@ -11,6 +12,27 @@ namespace ComicBookStoreAPI.Services
         public AccountService(IConfiguration aConfiguration)
         {
             _config = aConfiguration;
+        }
+
+        public string GenerateTemporaryPassword(string name, string lastName)
+        {
+            StringBuilder passwordBuilder = new StringBuilder();
+
+            var namePart = name.Trim();
+
+            passwordBuilder.Append(char.ToUpper(namePart[0]));
+            passwordBuilder.Append(namePart.Substring(1, 2));
+            passwordBuilder.Append("!");
+
+            var lastNamePart = lastName.Trim();
+            passwordBuilder.Append(char.ToUpper(lastNamePart[0]));
+            passwordBuilder.Append(lastNamePart.Substring(1, 2));
+
+            Random rnd = new Random();
+
+            passwordBuilder.Append(rnd.Next(10, 99));
+
+            return passwordBuilder.ToString();
         }
 
         public async Task<GoogleJsonWebSignature.Payload> VerifyGoogleToken(ExternalAuthDto externalAuth)
