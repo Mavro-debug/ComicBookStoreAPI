1efd60d4dfd95934cd3b88720762330e5b469908
Paweł Janus <82516385+PawelJanus99@users.noreply.github.com>
2022-06-02 00:37:49 +0200

    Added GeneratePassword method

--- ComicBookStoreAPI.Services/TemporaryUserPasswordGenerator.cs
+++ ComicBookStoreAPI.Services/TemporaryUserPasswordGenerator.cs
@@ -1,7 +1,28 @@
-﻿namespace ComicBookStoreAPI.Services
-{
-    public class TemporaryUserPasswordGenerator
-    {
+﻿using System.Text;
 
+namespace ComicBookStoreAPI.Services
+{
+    public static class TemporaryUserPasswordGenerator
+    {
+        public static string GeneratePassword(string name, string lastName)
+        {
+            StringBuilder passwordBuilder = new StringBuilder();
+
+            var namePart = name.Trim();
+
+            passwordBuilder.Append(char.ToUpper(namePart[0]));
+            passwordBuilder.Append(namePart.Substring(1, 2));
+            passwordBuilder.Append("!");
+
+            var lastNamePart = lastName.Trim();
+            passwordBuilder.Append(char.ToUpper(lastNamePart[0]));
+            passwordBuilder.Append(lastNamePart.Substring(1, 2));
+
+            Random rnd = new Random();
+
+            passwordBuilder.Append(rnd.Next(10, 99));
+
+            return passwordBuilder.ToString();
+        }
     }
 }
