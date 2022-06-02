745fb2f206be5f7be976f0aaf4fc7092315d8b8e
Paweł Janus <82516385+PawelJanus99@users.noreply.github.com>
2022-06-02 14:38:48 +0200

    Added IUserContextService interface

---
+++ ComicBookStoreAPI.Domain/Interfaces/Services/IUserContextService.cs
@@ -0,0 +1,11 @@
+﻿using System.Security.Claims;
+
+
+namespace ComicBookStoreAPI.Domain.Interfaces.Services
+{
+    public interface IUserContextService
+    {
+        string? GetUserId { get; }
+        ClaimsPrincipal User { get; }
+    }
+}
--- ComicBookStoreAPI.Services/UserContextService.cs
+++ ComicBookStoreAPI.Services/UserContextService.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using ComicBookStoreAPI.Domain.Interfaces.Services;
+using Microsoft.AspNetCore.Http;
 using System.Security.Claims;
 
 namespace ComicBookStoreAPI.Services
